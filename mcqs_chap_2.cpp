255f8494bcedf4c7b3933041d9d99437475e2eb7
NumanWali <numanwali08182@gmail.com>
2024-08-18 16:18:14 +0500

    Add files via upload

---
+++ mcqs_chap_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+// (1)
+int x=10, y=20;
+if(x==y);
+cout<<x<<""<<y;    
+//be consontrated which one is out put
+
+
+//(2)
+int main()
+int x=3,y,z;
+y=x=10;
+z=x<10;
+cout<<x<<""<<y<<""<<z;
+}
